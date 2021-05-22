fdfe640900a66a9ad06ca15ac5dd82ea801d5469
Jatin Sharma <65252998+rootjatin@users.noreply.github.com>
2021-05-22 13:01:48 +0530

    point location

---
+++ geometry/pointLocation.py
@@ -0,0 +1,18 @@
+a=int(input())
+aa=[]
+for i in range(a):
+	aa.append([int (x) for x in input().split()])
+
+for i in range(a):
+        aa[i][2]=aa[i][2]-aa[i][0]
+        aa[i][3]=aa[i][3]-aa[i][1]
+        aa[i][4]=aa[i][4]-aa[i][0]
+        aa[i][5]=aa[i][5]-aa[i][1]
+for i in range(a):          
+        p=aa[i][2]*aa[i][5]-(aa[i][3]*aa[i][4])
+        if(p==0):
+            print("TOUCH")
+        elif(p>0):
+            print("LEFT")
+        else:
+            print("RIGHT")
