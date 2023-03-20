7e559f55680f7b10f03f8b8208c2c332bf2140d0
Jatin Sharma <65252998+rootjatin@users.noreply.github.com>
2023-03-20 19:28:16 +0530

    Update can place flowers.cpp

--- greedy/can place flowers.cpp
+++ greedy/can place flowers.cpp
@@ -4,8 +4,8 @@ public:
         flowerbed.insert(flowerbed.begin(),0);
         flowerbed.push_back(0);
         int temp=0 ; 
-        for (int i=1; i<flowerbed.size()-2; i++){
-            if (flowerbed[i-1]==0 && flowerbed[i+1]==0){
+        for (int i=1; i<flowerbed.size()-1; i++){
+            if (flowerbed[i-1]==0 && flowerbed[i+1]==0 && flowerbed[i]!=1 ){
                 temp+=1;
                 flowerbed[i]=1;
             }
