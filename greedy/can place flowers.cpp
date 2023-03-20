7b6b2247536f417287839e99da65ead9d49d10bc
Jatin Sharma <65252998+rootjatin@users.noreply.github.com>
2023-03-20 19:19:19 +0530

    Create can place flowers.cpp

---
+++ greedy/can place flowers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        flowerbed.insert(flowerbed.begin(),0);
+        flowerbed.push_back(0);
+        int temp=0 ; 
+        for (int i=1; i<flowerbed.size()-2; i++){
+            if (flowerbed[i-1]==0 && flowerbed[i+1]==0){
+                temp+=1;
+                flowerbed[i]=1;
+            }
+        }
+        if (temp>=n){
+            cout<<temp; 
+            return true;
+        }
+
+        return false;
+    }
+};
